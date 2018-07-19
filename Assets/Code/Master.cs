4cf4719316c9c2f2ae5ee2a4d1038ead3920a0c2
Andrew Friedman <andrewfriedman01@gmail.com>
2018-07-19 19:18:19 -0400

    1.01
    *Fixed random generation of predictions
    *Fixed predictions.txt spacing

--- Assets/Code/Master.cs
+++ Assets/Code/Master.cs
@@ -2,13 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
-using System;
 
 public class Master : MonoBehaviour {
 	public TextAsset Predictions;
-	public GameObject Prediction;
+	public GameObject Prediction_Prefab;
 	public GameObject Instructions;
 	GameObject Clone;
+	string Prediction;
 
 	void Update () {
 		if (Input.GetMouseButtonDown(0)) {
@@ -16,12 +16,15 @@ public class Master : MonoBehaviour {
 				DestroyImmediate(Instructions, true);
 			}
 			Destroy(Clone);
-			Clone = Instantiate(Prediction, this.gameObject.transform);
+			Clone = Instantiate(Prediction_Prefab, this.gameObject.transform);
 			int Text_Length = Predictions.text.Split('\n').Length;
+			Debug.Log(Random.Range(0, Text_Length));
 			StreamReader SR = new StreamReader(new MemoryStream(Predictions.bytes));
-			for (int i = 0; i <= UnityEngine.Random.Range(0, Text_Length); i++) {
-				Prediction.GetComponent<UnityEngine.UI.Text>().text = SR.ReadLine();
+			int Random_Number = Random.Range(0, Text_Length);
+			for (int i = 0; i <= Random_Number; i++) {
+				Prediction = SR.ReadLine();
 			}
+			Clone.GetComponent<UnityEngine.UI.Text>().text = Prediction; 
 			SR.Close();
 		}
 	}
