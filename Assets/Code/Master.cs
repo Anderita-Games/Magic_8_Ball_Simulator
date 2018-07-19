65ce963fb13a0da25e54d196362cf6712275fece
Andrew Friedman <andrewfriedman01@gmail.com>
2018-07-19 18:25:59 -0400

    Added App
    This is the first version that GitHub will see of Magic 8 Ball Simulator!

---
+++ Assets/Code/Master.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System;
+
+public class Master : MonoBehaviour {
+	public TextAsset Predictions;
+	public GameObject Prediction;
+	public GameObject Instructions;
+	GameObject Clone;
+
+	void Update () {
+		if (Input.GetMouseButtonDown(0)) {
+			if (Instructions) {
+				DestroyImmediate(Instructions, true);
+			}
+			Destroy(Clone);
+			Clone = Instantiate(Prediction, this.gameObject.transform);
+			int Text_Length = Predictions.text.Split('\n').Length;
+			StreamReader SR = new StreamReader(new MemoryStream(Predictions.bytes));
+			for (int i = 0; i <= UnityEngine.Random.Range(0, Text_Length); i++) {
+				Prediction.GetComponent<UnityEngine.UI.Text>().text = SR.ReadLine();
+			}
+			SR.Close();
+		}
+	}
+}
